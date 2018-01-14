76daa37b47a3f3732d1fa533194a330960dad968
Kaley Kriminger <kaley.kriminger@gmail.com>
2018-01-14 11:25:56 -0800

    serves static html file, emits paint event to all sockets

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const port = process.env.port || 8080;
+const io = require('socket.io')(http);
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+http.listen(port, () => {
+  console.log(`Server listening on ${port}`);
+});
+
+io.on('connection', (socket) => {
+  socket.on('paint', (paintData) => {
+    console.log(paintData);
+    io.emit('paint', {
+      index : paintData.index,
+      color : paintData.color
+    });
+  });
+});
